df3bfb624bbbe8fe77fe7fb7962b22b9dc9c979b
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-22 00:18:02 -0400

    finished first part 1/5 done taking a break

--- Source.asm
+++ Source.asm
@@ -12,26 +12,75 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 ; DATA SEGMENT
 .data
-integer_1			DWORD		10
-integer_1Text		BYTE		"The value in 'integer_1' is :"		
-integer_2			DWORD		50
-integer_2Text		BYTE		"The value in 'integer_2' is :"		
-byte_1				BYTE		30
-byte_1Text			byte		"The value in 'byte_1' is    :"
-byte_2				BYTE		70
-byte_2Text			byte		"The value in 'byte_2' is    :"
+integer_1			DWORD		10d
+integer_1Text		BYTE		"The value in 'integer_1' is :", 0	
+integer_2			DWORD		50d
+integer_2Text		BYTE		"The value in 'integer_2' is :"	,0	
+byte_1				BYTE		30d
+byte_1Text			byte		"The value in 'byte_1' is   :",0
+byte_2				BYTE		70d
+byte_2Text			byte		"The value in 'byte_2' is   :",0
 intermediate_1		DWORD		0
 intermediate_2		DWORD		0
 intermediate_3		DWORD		0
-
+phrase_plus			BYTE		"+", 0
 ; CODE SEGMENT
 .code
 main PROC
 	
+	MOV EDX, offset integer_1Text
+	CALL WriteString
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	MOV EAX, 10d
+	CALL WriteDec
+	call crlf
 
-	; Return to OS.
+	mov edx, offset integer_2Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	MOV EAX, 50d
+	CALL WriteDec
+	call crlf
+
+
+	mov edx, offset byte_1
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	MOV EAX, 30d
+	CALL WriteDec
+	call crlf
+
+	mov edx, offset byte_2
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	MOV EAX, 70d
+	CALL WriteDec
+	call crlf
+	call crlf
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	INVOKE ExitProcess, 0
 main ENDP
+
+
+
+
+
+
+
+
 
 showOperandContents PROC
 	; YOUR CODE GOES HERE...
