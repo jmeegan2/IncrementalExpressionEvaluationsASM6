fcad94dff7bb5092ecf99a85ccb812de60bc1c70
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-23 23:14:08 -0400

    added a bunch of variable to the .data section to make an outline for the program then ill go in and do the problems

--- Source.asm
+++ Source.asm
@@ -14,29 +14,48 @@ ExitProcess PROTO, dwExitCode:DWORD
 .data
 integer_1			DWORD		10d, 0
 integer_1Text		BYTE		"The value in 'integer_1' is    : ", 0	
+
 integer_2			DWORD		50d , 0
 integer_2Text		BYTE		"The value in 'integer_2' is    : ",0	
+
 byte_1				BYTE		30d
 byte_1Text			BYTE		"The value in 'byte_1' is      : ",0
+
 byte_2				BYTE		70d
 byte_2Text			BYTE		"The value in 'byte_2' is      : ",0
+
 phrasePracticeOne	byte		"Practice Expression #01",0
 phrasePracticeTwo	byte		"Practice Expression #02",0
+phrasePracticeThree	byte		"Practice Expression #03",0
+phrasePracticeFour	byte		"Practice Expression #04",0
 line				byte		"------------------------------------" ,0
+
 practiceOneARM		byte		"-(integer_1 + integer_2)", 0			;ARM just means arithmetic 
 practiceOneARMpart2	byte		"-(intermediate_1)", 0
+
 practiceTwoARM		byte		"integer_2 - integer_1 + 2(integer_2)", 0			;ARM just means arithmetic 
 practiceTwoARMpart2	byte		"integer_2 - integer_1 + intermediate_1", 0
 practiceTwoARMpart3	byte		"intermediate_2 + intemediate_1", 0
+
+practiceThreeARM    byte		"integer_1 + byte_1 - integer_2",0
+practiceThreeARMpart2	byte	"intermediate_1 - integer_2",0
+
+practiceFourARM			byte		"byte_2 - byte_1 + 2(integer_2 - byte_1)",0
+practiceFourARMpart2	byte		"byte_2 - byte_1 + 2(intermediate_1)",0
+practiceFourARMpart3	byte		"byte_2 - byte_1 + intermediate_2",0
+practiceFourARMpart4	byte	"intermediate_3 + intermediate _2",0
+
 intermediate_1		BYTE		? 
 intermediate_1Text  byte		"The value in 'intermiate_1' is :", 0
 intermediate_2		byte		?
 intermediate_2Text  byte		"The value in 'intermiate_2' is :", 0
 intermediate_3		byte		?
 intermediate_3Text  byte		"The value in 'intermiate_3' is :", 0
+
 phrase_plus			BYTE		"+", 0
 phrase_minus		BYTE		"-", 0
-imABYTEVariable					BYTE	10d
+
+
 ; CODE SEGMENT
 .code
 main PROC
@@ -185,7 +204,61 @@ main PROC
 	call writedec
 	call crlf
 	call crlf
+	; start of part two of practice #2
+	mov edx, offset practiceTwoARMpart2  ;//integer_2 - integer_1 +2(integer_2//
+	call writestring
+	call crlf
+	mov edx, offset intermediate_1Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_1
+	call writedec
+	call crlf
+	mov edx, offset intermediate_2Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_2
+	call writedec
+	call crlf
+	mov edx, offset intermediate_3Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_3
+	call writedec
 	
+	
+	call crlf
+	call crlf
+	; start of part three of practice #2
+	mov edx, offset practiceTwoARMpart3  ;//intermediate_2 + intermediate_1//
+	call writestring
+	call crlf
+	mov edx, offset intermediate_1Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_1
+	call writedec
+	call crlf
+	mov edx, offset intermediate_2Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_2
+	call writedec
+	call crlf
+	mov edx, offset intermediate_3Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_3
+	call writedec
+	
+	
+	call crlf
 	
 	
 	
