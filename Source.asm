0f618f12125a346bb66ab8dfb852dacb4f40e4ce
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-25 21:41:00 -0400

    outline with some values added created

--- Source.asm
+++ Source.asm
@@ -376,6 +376,56 @@ main PROC
 	call writedec
 	call crlf 
 	call crlf
+	mov edx, offset practiceFourARMpart3 ;//intermediate_2 + intermediate_1//
+	call writestring
+	call crlf 
+	mov edx, offset intermediate_1Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_1
+	call writedec
+	call crlf
+	mov edx, offset intermediate_2Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_2
+	call writedec
+	call crlf
+	mov edx, offset intermediate_3Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_3
+	call writedec
+	call crlf 
+	call crlf
+	mov edx, offset practiceFourARMpart4 ;//intermediate_2 + intermediate_1//
+	call writestring
+	call crlf 
+	mov edx, offset intermediate_1Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_1
+	call writedec
+	call crlf
+	mov edx, offset intermediate_2Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_2
+	call writedec
+	call crlf
+	mov edx, offset intermediate_3Text
+	call writestring
+	MOV EDX, OFFSET phrase_plus
+	CALL WriteString
+	movzx eax, intermediate_3
+	call writedec
+	call crlf 
+	call crlf
 	call crlf 
 	INVOKE ExitProcess, 0
 main ENDP
